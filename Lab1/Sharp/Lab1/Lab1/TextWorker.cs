b39a4115acc016cd31c32695d8549edc83c12264
Vlad Golovatyuk <vladd.golovatyuk@gmail.com>
2022-04-05 14:47:38 +0300

    excluding first array element from second one

--- Lab1/Sharp/Lab1/Lab1/TextWorker.cs
+++ Lab1/Sharp/Lab1/Lab1/TextWorker.cs
@@ -8,6 +8,9 @@ namespace Lab1
 {
     internal class TextWorker
     {
-        
+        public string[] SecondWithoutFirst(string[] first, string[] second)
+        {
+            return second.Except(first).ToArray();
+        }
     }
 }
