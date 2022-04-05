243c7fc547d949ec29d1c7f21c7ba76314a68d75
Vlad Golovatyuk <vladd.golovatyuk@gmail.com>
2022-04-05 13:32:03 +0300

    file worker methods

--- Lab1/Sharp/Lab1/Lab1/FileWorker.cs
+++ Lab1/Sharp/Lab1/Lab1/FileWorker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,35 @@ namespace Lab1
 {
     internal class FileWorker
     {
-
+        /// <summary>
+        /// Creates file in executable file directory
+        /// </summary>
+        /// <param name="fileName">File name without extension</param>
+        public void CreateFile(string fileName)
+        {
+            File.Create($"{fileName}.txt").Close();
+        }
+        /// <summary>
+        /// Rewrites file with the given content
+        /// </summary>
+        /// <param name="fileName">File name without extension</param>
+        /// <param name="data">Content</param>
+        public void WriteData(string fileName, string[] data)
+        {
+            if (File.Exists($"{fileName}.txt") == false)
+                CreateFile($"{fileName}.txt");
+            File.WriteAllLines($"{fileName}.txt", data);
+        }
+        /// <summary>
+        /// Append file with the given content
+        /// </summary>
+        /// <param name="fileName">File name without extension</param>
+        /// <param name="data">Content</param>
+        public void AppendData(string fileName, string[] data)
+        {
+            if (File.Exists($"{fileName}.txt") == false)
+                CreateFile($"{fileName}.txt");
+            File.AppendAllLines($"{fileName}.txt", data);
+        }
     }
 }
