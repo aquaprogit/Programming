9ec7e308226858d29b1e7e1f9eab3df0d7514fcf
Vlad Golovatyuk <vladd.golovatyuk@gmail.com>
2022-04-05 14:46:08 +0300

    console multiline input

--- Lab1/Sharp/Lab1/Lab1/ConsoleWorker.cs
+++ Lab1/Sharp/Lab1/Lab1/ConsoleWorker.cs
@@ -8,6 +8,45 @@ namespace Lab1
 {
     internal class ConsoleWorker
     {
-
+        /// <summary>
+        /// Provides user multiline input while exit key + ALT are not pressed 
+        /// </summary>
+        /// <param name="exitKey">Key to exit</param>
+        /// <returns>Array of inputted lines</returns>
+        public string[] GetMultilineInput(ConsoleKey exitKey = ConsoleKey.E)
+        {
+            ConsoleKeyInfo key;
+            string result = "";
+            bool keepEntering;
+            do
+            {
+                key = Console.ReadKey();
+                keepEntering = key.Key != exitKey || key.Modifiers != ConsoleModifiers.Alt;
+                if (keepEntering)
+                {
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.Enter:
+                            result += "\n";
+                            Console.CursorTop++;
+                            break;
+                        case ConsoleKey.Backspace:
+                            Console.Write(' ');
+                            Console.CursorLeft--;
+                            break;
+                        default:
+                            result += key.KeyChar.ToString();
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.CursorLeft--;
+                    Console.Write(' ');
+                    Console.WriteLine();
+                }
+            } while (keepEntering);
+            return result.Split('\n');
+        }
     }
 }
