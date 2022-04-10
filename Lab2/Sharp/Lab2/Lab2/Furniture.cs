8be6c0bbd8ee082c856759b2e7a2503351d30653
Vlad Golovatyuk <vladd.golovatyuk@gmail.com>
2022-04-11 00:58:23 +0300

    furniture data container

---
+++ Lab2/Sharp/Lab2/Lab2/Furniture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Furniture
+    {
+        public Furniture(string name, string type, int amount, int price, Color color)
+        {
+            Name = name;
+            Type = type;
+            Amount = amount;
+            Price = price;
+            Color = color;
+        }
+        public string Name { get; private set; }
+        public string Type { get; private set; }
+        public Color Color { get; private set; }
+        public int Price { get; private set; }
+        public int Amount { get; private set; }
+
+    }
+    enum Color
+    {
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        Orange,
+        Purple,
+        Brown,
+        White,
+        Black
+    }
+}
